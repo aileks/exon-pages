ebf7a863f3399f40f57bf0d0d61139871f0ffbcc
Aaliyah Harper <aeverly14@pm.me>
2025-04-13 19:49:41 -0400

    add metadata arg

--- app/models/db.py
+++ app/models/db.py
@@ -6,4 +6,4 @@ from flask_sqlalchemy import SQLAlchemy
 environment: str = os.environ.get("FLASK_ENV") or "dev"
 SCHEMA: str | None = os.environ.get("SCHEMA")
 metadata = MetaData(schema=SCHEMA)
-db = SQLAlchemy()
+db = SQLAlchemy(metadata=metadata)
