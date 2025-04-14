d4cd6253d830739fd71be8fec49bcad0cc2a6a69
Aaliyah Harper <aeverly14@pm.me>
2025-04-13 20:19:25 -0400

    update config.py

--- app/config.py
+++ app/config.py
@@ -9,9 +9,9 @@ class Config:
     FLASK_RUN_PORT: str | int = os.environ.get("FLASK_RUN_PORT", 8000)
 
     SQLALCHEMY_TRACK_MODIFICATIONS: bool = False
-    db_url: str | None = os.environ.get("DATABASE_URL")
+    db_url: str = os.environ.get("DATABASE_URL")
     if db_url and db_url.startswith("postgres://"):
         SQLALCHEMY_DATABASE_URI: str = db_url.replace("postgres://", "postgresql://")
     else:
-        SQLALCHEMY_DATABASE_URI: str = db_url or "sqlite:///dev.db"
+        SQLALCHEMY_DATABASE_URI: str = db_url
     SQLALCHEMY_ECHO: bool = True
