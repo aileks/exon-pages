16cf76a3505099b5c7e403e360d295dcbcf265fb
Aaliyah Harper <aeverly14@pm.me>
2025-04-13 20:28:28 -0400

    add heroku.yml

--- app/config.py
+++ app/config.py
@@ -2,14 +2,10 @@ import os
 
 
 class Config:
-    SECRET_KEY = (
-        os.environ.get("SECRET_KEY")
-        or "app-dev-key-that-should-not-be-used-in-production"
-    )
+    SECRET_KEY = os.environ.get("SECRET_KEY")
     FLASK_RUN_PORT: str | int = os.environ.get("FLASK_RUN_PORT", 8000)
-
+    SQLALCHEMY_ECHO = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL").replace(  # pyright: ignore
         "postgres://", "postgresql://"
     )
-    SQLALCHEMY_ECHO = True
